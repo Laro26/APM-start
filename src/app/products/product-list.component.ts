aa84cea4618286a8315d891401e43227c860c1a5
Laro26 <laladelaro26@hotmail.com>
2021-01-31 12:04:19 -0500

    App hecha por Deborah

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,58 @@
+import {Component, OnInit} from '@angular/core';
+import {IProduct} from './product';
+import {ProductService} from './product.service';
+
+@Component ({
+  templateUrl:'./product-list.component.html',
+  styleUrls:['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+pageTitle: string='Product List';
+pageTitlee: string='Rating:';
+imageWidth: number=50;
+imageMargin: number=2;
+showImage: boolean= false;
+errorMessage: string;
+
+_listFilter: string;
+get listFilter(): string {
+  return this._listFilter;
+}
+set listFilter(value: string) {
+this._listFilter = value;
+  this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+}
+
+//Filtrar Productos
+filteredProducts: IProduct[] = [];
+products: IProduct[]= [];
+
+constructor (private productService: ProductService){
+  
+//rating
+}
+performFilter(filterBy: string): IProduct[] {
+  filterBy = filterBy.toLocaleLowerCase();
+  return this.products.filter((product: IProduct) =>
+    product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+}
+
+onReatingClicked(message: string): void{
+  this.pageTitlee= 'Rating' + message;
+}
+
+toggleImage(): void{
+  this.showImage= ! this.showImage
+}
+ngOnInit(): void{
+  this.productService.getProducts().subscribe({
+    next: products => {
+      this.products= products;
+      this.filteredProducts=this.products
+    },
+    error:err=> this.errorMessage=err
+  });
+
+}
+
+}
